c8c80c436df8b1a05914c320152e75cece2556b6
OrochimaruBotz <109642842+OrochimaruBotz@users.noreply.github.com>
2022-10-12 13:52:42 +0700

    Update main-sgay.js

--- plugins/main-sgay.js
+++ plugins/main-sgay.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/b2342e43791d13bbd5eea.png', m, { packname: "zifabotz", author: "@rozi" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/b2342e43791d13bbd5eea.png', m, { packname: "GojoBotzMD", author: "@humanz" })
 }
 
 handler.customPrefix = /^(gay)$/i
